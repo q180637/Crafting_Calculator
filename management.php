c1c0b12242b441873238918ab533bc4b23b1aa08
Ben Richardson <q180637@selby.ac.uk>
2023-04-04 16:15:52 +0100

    now all weapons baseline

---
+++ management.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta author="Ben Richardson">
+            <meta name="description" content="">
+            <title></title>
+            <link href="css/main.css" rel="stylesheet">
+            <link rel="icon" href="img/destiny_logo.png" title="Destiny logo favicon" alt="Destiny logo favicon">
+    </head>
+    <body>
+        
+        
+    </body>
+</html>
