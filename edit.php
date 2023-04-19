58806910ab542fd3bf9e9e4aecb028729fcc913f
Ben Richardson <q180637@selby.ac.uk>
2023-04-19 15:17:11 +0100

    edit now retrieves a weapon info

--- edit.php
+++ edit.php
@@ -16,68 +16,6 @@ if(isset($_SESSION['username'])){
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
 
-        function loadWeaponForm(){
-            // header('Content-Type: application/json');
-        
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-        
-            $selected = $_REQUEST['recordList'];
-        
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, 'db_calculator_tool');
-        
-            $sql = "SELECT * FROM weapons";
-            $result = $conn->query($sql);
-
-            if($result->num_rows > 0){
-                $row = $result ->fetch_assoc();
-
-                $weapon_ID = $result['weapon_ID'];
-                $weapon_name = $result['weapon_name'];
-                $weapon_type = $result['weapon_type'];
-                $weapon_source = $result['weapon_source'];
-                $pattern_count = $result['pattern_count'];
-                $base_impact = $result['base_impact'];
-                $base_range = $result['base_range'];
-                $base_stability = $result['base_stability'];
-                $base_handling = $result['base_handling'];
-                $base_reload = $result['base_reload'];
-                $basvisibilitye_AA = $result['base_AA'];
-                $base_zoom = $result['base_zoom'];
-                $base_recoil = $result['base_recoil'];
-                $base_RPM = $result['base_RPM'];
-                $base_draw = $result['base_draw'];
-                $base_accuracy = $result['base_accuracy'];
-                $base_mag = $result['base_mag'];
-                $icon_file_path = $result['icon_file_path'];
-
-                echo
-                "<p style='visibility:hidden;' id='$weapon_name'>$weapon_name</p>
-                <p style='visibility:hidden;' id='$weapon_type'>$weapon_type</p>
-                <p style='visibility:hidden;' id='$weapon_source'>$weapon_source</p>
-                <p style='visibility:hidden;' id='$pattern_count'>$pattern_count</p>
-                <p style='visibility:hidden;' id='$base_impact'>$base_impact</p>
-                <p style='visibility:hidden;' id='$base_range'>$base_range</p>
-                <p style='visibility:hidden;' id='$base_stability'>$base_stability</p>
-                <p style='visibility:hidden;' id='$base_handling'>$base_handling</p>
-                <p style='visibility:hidden;' id='$base_reload'>$base_reload</p>
-                <p style='visibility:hidden;' id='$base_AA'>$base_AA</p>
-                <p style='visibility:hidden;' id='$base_zoom'>$base_zoom</p>
-                <p style='visibility:hidden;' id='$base_recoil'>$base_recoil</p>
-                <p style='visibility:hidden;' id='$base_RPM'>$base_RPM</p>
-                <p style='visibility:hidden;' id='$base_draw'>$base_draw</p>
-                <p style='visibility:hidden;' id='$base_accuracy'>$base_accuracy</p>
-                <p style='visibility:hidden;' id='$base_mag'>$base_mag</p>
-                <p style='visibility:hidden;' id='$icon_file_path'>$icon_file_path</p>
-                ";
-
-                return $result;
-            }
-        
-            mysqli_close($conn);
-        }
     ?>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -625,7 +563,7 @@ if(isset($_SESSION['username'])){
                 $.ajax(
                     {
                         contentType: 'application/json',
-                        method: 'POST',
+                        type: 'POST',
                         url: 'php/db_management_functionality.php',
                         data: "function=fillWeaponRecordList",
                     }
@@ -635,7 +573,7 @@ if(isset($_SESSION['username'])){
                 $.ajax(
                     {
                         contentType: 'application/json',
-                        method: 'POST',
+                        type: 'POST',
                         url: 'php/db_management_functionality.php',
                         data: "function=fillPerksRecordList",
                     }
@@ -645,7 +583,7 @@ if(isset($_SESSION['username'])){
                 $.ajax(
                     {
                         contentType: 'application/json',
-                        method: 'POST',
+                        type: 'POST',
                         url: 'php/db_management_functionality.php',
                         data: "function=fillLogonRecordList",
                     }
@@ -655,7 +593,7 @@ if(isset($_SESSION['username'])){
                 $.ajax(
                     {
                         contentType: 'application/json',
-                        method: 'POST',
+                        type: 'POST',
                         url: 'php/db_management_functionality.php',
                         data: "function=fillFrameRecordList",
                     }
@@ -665,7 +603,7 @@ if(isset($_SESSION['username'])){
                 $.ajax(
                     {
                         contentType: 'application/json',
-                        method: 'POST',
+                        type: 'POST',
                         url: 'php/db_management_functionality.php',
                         data: "function=fillBarrelRecordList",
                     }
@@ -675,7 +613,7 @@ if(isset($_SESSION['username'])){
                 $.ajax(
                     {
                         contentType: 'application/json',
-                        method: 'POST',
+                        type: 'POST',
                         url: 'php/db_management_functionality.php',
                         data: "function=fillMagRecordList",
                     }
@@ -685,7 +623,7 @@ if(isset($_SESSION['username'])){
                 $.ajax(
                     {
                         contentType: 'application/json',
-                        method: 'POST',
+                        type: 'POST',
                         url: 'php/db_management_functionality.php',
                         data: "function=fillTrait1RecordList",
                     }
@@ -695,7 +633,7 @@ if(isset($_SESSION['username'])){
                 $.ajax(
                     {
                         contentType: 'application/json',
-                        method: 'POST',
+                        type: 'POST',
                         url: 'php/db_management_functionality.php',
                         data: "function=fillTrait2RecordList",
                     }
@@ -707,18 +645,19 @@ if(isset($_SESSION['username'])){
         let r = document.getElementById('tableList');
         let record = r.value;
         function loadWeapons(){
-            // var xhttp = new XMLHttpRequest();
-            // xhttp.onreadystatechange = function(){
-            //     if(xhttp.readyState == 4 && xhttp.status == 200){
-            //         console.log(xhttp);
-            //     }
-            // };
-            // var functionName = "weapons";
-            // xhttp.open("POST", "php/db_management_functionality.php", true);
-            // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            // xhttp.send(functionName = r.value);
-            const load = <?php echo loadWeaponForm()?>;
-            document.getElementById("weapons").innerHTML = load;
+            $.ajax(
+                    {
+                        url: 'php/db_management_functionality.php',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: "function=loadWeapons",
+                        success: function(response){
+                            console.log("test");
+                            console.log(response);
+                        }
+                    }
+                )
+            document.getElementById
         }
     </script>
 </html>
